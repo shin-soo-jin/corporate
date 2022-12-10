519785fc3e0a6a7cdda0cafd0b824ba491e83dca
shin-soo-jin <1tnwls02@naver.com>
2022-12-10 23:00:21 +0900

    스크롤 효과 적용

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Swiper
 var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
@@ -11,12 +12,49 @@ var swiper = new Swiper(".mySwiper", {
 
 const btnCall = document.querySelector(".btnCall");
 const gnbMo = document.querySelector("#gnbMo");
-const menuWeb = document.querySelector(".menuWeb");
 
-btnCall.onclick = function (e) {
+const sections = document.querySelectorAll(".section");
+const scrollNavi = document.querySelectorAll(".scrollNavi li");
+
+let posArr = [];
+for (let el of sections) {
+  posArr.push(el.offsetTop);
+}
+// (6) [0, 937, 1227, 1681, 2269, 2632] 아닌디디디디디디디디디
+
+// mobile menu
+btnCall.addEventListener("click", (e) => {
   e.preventDefault();
 
   btnCall.classList.toggle("on");
   gnbMo.classList.toggle("on");
-}
+})
 
+// scroll
+window.addEventListener("scroll", () => {
+  let scroll = window.scrollY || window.pageYOffset;
+
+  // scrollNavi .on, sections .show
+  sections.forEach((el, index) => {
+    if (scroll >= posArr[index] - 500) {
+      scrollNavi.forEach((el, index) => {
+        el.classList.remove('on');
+      })
+      scrollNavi[index].classList.add('on');
+      sections[index].classList.add('show');
+    }
+  })
+
+  // scrollNavi click
+  scrollNavi.forEach((el, index) => {
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      new Anim(window, {
+        prop: 'scroll',
+        value: posArr[index],
+        duration: 500,
+      })
+    })
+  })
+})
